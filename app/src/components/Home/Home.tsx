48723250b7c55a47271f6409a9721d50493647e1
Antonin Riche <antonin@jetpack.ai>
2025-03-05 16:32:10 +0100

    ✨ Improve home page

--- app/src/components/Home/Home.tsx
+++ app/src/components/Home/Home.tsx
@@ -6,6 +6,9 @@ import NewGameForm from "../Game/NewGameForm";
 function Home() {
   return (
     <div>
+      <h2>Welcome on Travle !</h2>
+      <p>The goal of the game is to find the shortest path between two countries / territories.</p>
+      <p>Let&apos;s start a game !!!</p>
       <NewGameForm
         style={{
           backgroundColor: "#05050520",
